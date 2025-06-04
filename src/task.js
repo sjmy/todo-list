769679da0fc3ef2289b14957865b4d64ecfbda38
sjmy <23250749+sjmy@users.noreply.github.com>
2025-06-04 16:35:47 -0700

    more pseudocode, first crack at a Task object in task.js

---
+++ src/task.js
@@ -0,0 +1,61 @@
+// - Task
+//     - created via factory function
+//     - Properties:
+//         - taskID
+//             - crypto.randomUUID()
+//         - taskName
+//             - getter/setter
+//         - taskDescription
+//             - getter/setter
+//         - taskDueDate
+//             - getter/setter
+//         - taskPriority
+//             - getter/setter
+//         - taskNotes
+//             - getter/setter
+//         - checklist box
+//             - toggle
+//             - strikethrough when toggled
+
+export default function Task(taskName, taskDescription, taskDueDate, taskPriority, taskNotes) {
+    const taskID = crypto.randomUUID();
+    let taskName = taskName;
+    let taskDescription = taskDescription;
+    let taskDueDate = taskDueDate;
+    let taskPriority = taskPriority;
+    let taskNotes = taskNotes;
+
+    // Getters
+    const getTaskID = () => taskID;
+    const getTaskDescription = () => taskDescription;
+    const getTaskDueDate = () => taskDueDate;
+    const getTaskPriority = () => taskPriority;
+    const getTaskNotes = () => taskNotes;
+
+    // Setters
+    const setTaskDescription = (newTaskDescription) => {
+        taskDescription = newTaskDescription;
+    };
+
+    const setTaskDueDate = (newTaskDueDate) => {
+        taskDueDate = newTaskDueDate;
+    };
+
+    const setTaskPriority = (newTaskPriority) => {
+        taskPriority = newTaskPriority;
+    };
+
+    const setTaskNotes = (newTaskNotes) => {
+        taskNotes = newTaskNotes;
+    };
+
+    return {getTaskID,
+            getTaskDescription,
+            getTaskDueDate,
+            getTaskPriority,
+            getTaskNotes,
+            setTaskDescription,
+            setTaskDueDate,
+            setTaskPriority,
+            setTaskNotes};
+};
