4b385549c6806e51bfdbad36ac0d762c90d39ad0
sjmy <23250749+sjmy@users.noreply.github.com>
2025-06-06 22:24:41 -0700

    drawProject() and drawTask() both working, added some styling

--- src/screen-controller.js
+++ src/screen-controller.js
@@ -46,29 +46,65 @@ export default function ScreenControllerObject() {
     const drawProject = (project) => {
         const mainContent = document.querySelector(".main");
         const projectName = document.createElement("h1");
+        const projectDescriptionDiv = document.createElement("div");
         const projectDescription = document.createElement("textarea");
         const projectItems = document.createElement("div");
         const projectDueDate = document.createElement("span");
         const projectPriority = document.createElement("span");
+        const projectTasks = project.getProjectTasks();
+        const taskItems = document.createElement("div");
 
         clearMainContent();
 
+        projectDescriptionDiv.classList.add("projectDescription");
+        projectDescription.classList.add(`${project.getProjectID()}`);
         projectItems.classList.add("projectItems");
-        // Add a class and/or name to textarea
+        taskItems.classList.add("taskItems");
+
+        // This event listener copies the project description to an attribute on the projectDescriptionDiv
+        // In styles.css, a hidden pseudo-element is overlayed on projectDescription so the textarea matches the content
+        // https://chriscoyier.net/2023/09/29/css-solves-auto-expanding-textareas-probably-eventually/
+        projectDescription.addEventListener("input", () => {
+            projectDescriptionDiv.dataset.replicatedValue = projectDescription.value;
+        });
 
         projectName.textContent = project.getProjectName();
         projectDescription.textContent = project.getProjectDescription();
         projectDueDate.textContent = `Due: ${project.getProjectDueDate()}`;
-        projectPriority.textContent = project.getProjectPriority();
+        projectPriority.textContent = `${project.getProjectPriority()} priority`;
 
+        projectDescriptionDiv.appendChild(projectDescription);
         projectItems.appendChild(projectDueDate);
         projectItems.appendChild(projectPriority);
         mainContent.appendChild(projectName);
-        mainContent.appendChild(projectDescription);
         mainContent.appendChild(projectItems);
+        mainContent.appendChild(projectDescriptionDiv);
+        mainContent.appendChild(taskItems);
+
+        for (let t = 0; t < projectTasks.length; t++) {
+            drawTask(projectTasks[t]);
+        };
+    };
+
+    const drawTask = (task) => {
+        const taskItems = document.querySelector(".taskItems");
+        const taskDiv = document.createElement("div");
+        const taskLabel = document.createElement("label");
+        const checkbox = document.createElement("input");
+
+        taskDiv.classList.add(`${task.getTaskID()}`);
+        taskLabel.htmlFor = `${task.getTaskID()}`;
+        checkbox.id = `${task.getTaskID()}`;
+        taskLabel.textContent = task.getTaskName();
+        checkbox.type = "checkbox";
+
+        taskDiv.appendChild(checkbox);
+        taskDiv.appendChild(taskLabel);
+        taskItems.appendChild(taskDiv);
     };
 
     return {clearMainContent,
-            drawProject
+            drawProject,
+            drawTask
     };
 };
